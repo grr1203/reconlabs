39ab8d72a2d199a82ab7eac908e3f368f6d9cc0e
dolanap <gyrms9412@naver.com>
2022-05-20 00:06:24 +0900

    헤더 부분 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import logo from "./assets/img/logo.png";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <div className="header">
+        <img className="header__logo" src={logo} alt="logo" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
