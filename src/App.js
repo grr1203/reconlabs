93707b4efa1f242c728cebe86a2341b676f92d75
dolanap <gyrms9412@naver.com>
2022-05-20 11:37:13 +0900

    - index.html, App.js 불필요 부분 제거 - App.css 파일 이동

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import ModelViewer from "./components/ModelViewer";
-import "./App.css";
+import "./assets/css/App.css";
 import logo from "./assets/img/logo.png";
 
 function App() {
   return (
     <div className="App">
       <div className="header">
-        <i className="fa-solid fa-circle-exclamation"></i>
         <img className="header__logo" src={logo} alt="logo" />
       </div>
       <ModelViewer />
